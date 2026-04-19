5fe0544835ef1f1148f9c67e1937c41e28c254d2
AdamCastello <adam.salvador.com@gmail.com>
2026-04-20 05:11:58 +0700

    Add files via upload

---
+++ script/W18.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btnW18 = document.getElementById('btnW18');
+    if (btnW18) {
+        btnW18.addEventListener('click', function() {
+            window.open('https://bt-kepsss.vercel.app', '_blank');
+        });
+    }
+});
